0d9303ebfb30ede85adcca003192f776ad954a3c
Ranjeet Kumar <ranjeet030200@gmail.com>
2023-08-06 20:26:57 +0530

    Assignment done

---
+++ spotDraft/src/main/java/com/example/spotDraft/SpotDraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotDraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotDraftApplication.class, args);
+	}
+
+}
